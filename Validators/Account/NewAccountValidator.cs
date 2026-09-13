305d3253ec1491a7956ccebc34e90fe5975a52ca
JJTransportes <comercialpepiviagens@gmail.com>
2026-09-13 12:15:50 -0300

    chore: update user type property.

--- Validators/Account/NewAccountValidator.cs
+++ Validators/Account/NewAccountValidator.cs
@@ -8,7 +8,7 @@ internal sealed class NewAccountValidator : AbstractValidator<NewAccountDto>
     public NewAccountValidator()
     {
         RuleFor(x => x.UserType)
-            .NotNull().WithMessage("O tipo de usuário deve ser informado.");
+            .IsInEnum().WithMessage("Tipo de usuário selecionado inválido.");
         RuleFor(x => x.Password)
             .MinimumLength(8).WithMessage("A senha precisa ter pelo menos 8 dígitos.");
         RuleFor(x => x.Email)
